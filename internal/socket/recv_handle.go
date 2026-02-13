321c7f13590bf922944e6ff11be8e8157cebb441
deramil <miladkhodaverdi@proton.me>
2026-02-13 22:43:57 +0200

    Optimize packet parsing and serialization hot paths

--- internal/socket/recv_handle.go
+++ internal/socket/recv_handle.go
@@ -12,7 +12,15 @@ import (
 )
 
 type RecvHandle struct {
-	handle *pcap.Handle
+	handle  *pcap.Handle
+	parser  *gopacket.DecodingLayerParser
+	decoded []gopacket.LayerType
+	eth     layers.Ethernet
+	ipv4    layers.IPv4
+	ipv6    layers.IPv6
+	tcp     layers.TCP
+	udp     layers.UDP
+	payload gopacket.Payload
 }
 
 func NewRecvHandle(cfg *conf.Network) (*RecvHandle, error) {
@@ -33,7 +41,19 @@ func NewRecvHandle(cfg *conf.Network) (*RecvHandle, error) {
 		return nil, fmt.Errorf("failed to set BPF filter: %w", err)
 	}
 
-	return &RecvHandle{handle: handle}, nil
+	r := &RecvHandle{handle: handle}
+	r.parser = gopacket.NewDecodingLayerParser(
+		layers.LayerTypeEthernet,
+		&r.eth,
+		&r.ipv4,
+		&r.ipv6,
+		&r.tcp,
+		&r.udp,
+		&r.payload,
+	)
+	r.decoded = make([]gopacket.LayerType, 0, 8)
+
+	return r, nil
 }
 
 func (h *RecvHandle) Read() ([]byte, net.Addr, error) {
@@ -41,38 +61,30 @@ func (h *RecvHandle) Read() ([]byte, net.Addr, error) {
 	if err != nil {
 		return nil, nil, err
 	}
-	p := gopacket.NewPacket(data, layers.LayerTypeEthernet, gopacket.NoCopy)
+	h.decoded = h.decoded[:0]
+	_ = h.parser.DecodeLayers(data, &h.decoded)
+	if len(h.decoded) == 0 {
+		return nil, nil, nil
+	}
 
 	addr := &net.UDPAddr{}
-
-	netLayer := p.NetworkLayer()
-	if netLayer == nil {
-		return nil, nil, nil
+	for _, t := range h.decoded {
+		switch t {
+		case layers.LayerTypeIPv4:
+			addr.IP = h.ipv4.SrcIP
+		case layers.LayerTypeIPv6:
+			addr.IP = h.ipv6.SrcIP
+		case layers.LayerTypeTCP:
+			addr.Port = int(h.tcp.SrcPort)
+		case layers.LayerTypeUDP:
+			addr.Port = int(h.udp.SrcPort)
+		}
 	}
-	switch netLayer.LayerType() {
-	case layers.LayerTypeIPv4:
-		addr.IP = netLayer.(*layers.IPv4).SrcIP
-	case layers.LayerTypeIPv6:
-		addr.IP = netLayer.(*layers.IPv6).SrcIP
-	}
-
-	trLayer := p.TransportLayer()
-	if trLayer == nil {
-		return nil, nil, nil
-	}
-	switch trLayer.LayerType() {
-	case layers.LayerTypeTCP:
-		addr.Port = int(trLayer.(*layers.TCP).SrcPort)
-	case layers.LayerTypeUDP:
-		addr.Port = int(trLayer.(*layers.UDP).SrcPort)
-	}
-
-	appLayer := p.ApplicationLayer()
-	if appLayer == nil {
+	if addr.IP == nil || addr.Port == 0 || len(h.payload) == 0 {
 		return nil, nil, nil
 	}
 
-	return appLayer.Payload(), addr, nil
+	return h.payload, addr, nil
 }
 
 func (h *RecvHandle) Close() {
--- internal/socket/send_handle.go
+++ internal/socket/send_handle.go
@@ -23,6 +23,11 @@ type TCPF struct {
 	mu         sync.RWMutex
 }
 
+var (
+	tcpOptMSSData         = []byte{0x05, 0xb4}
+	tcpOptWindowScaleData = []byte{8}
+)
+
 type SendHandle struct {
 	handle      *pcap.Handle
 	txProto     string
@@ -31,8 +36,7 @@ type SendHandle struct {
 	srcIPv6     net.IP
 	srcIPv6RHWA net.HardwareAddr
 	srcPort     uint16
-	synOptions  []layers.TCPOption
-	ackOptions  []layers.TCPOption
+	bufOptions  gopacket.SerializeOptions
 	time        uint32
 	tsCounter   uint32
 	tcpF        TCPF
@@ -50,65 +54,29 @@ func NewSendHandle(cfg *conf.Network) (*SendHandle, error) {
 		return nil, fmt.Errorf("failed to open pcap handle: %w", err)
 	}
 
-	// SetDirection is not fully supported on Windows Npcap, so skip it
 	if runtime.GOOS != "windows" {
 		if err := handle.SetDirection(pcap.DirectionOut); err != nil {
 			return nil, fmt.Errorf("failed to set pcap direction out: %v", err)
 		}
 	}
 
-	synOptions := []layers.TCPOption{
-		{OptionType: layers.TCPOptionKindMSS, OptionLength: 4, OptionData: []byte{0x05, 0xb4}},
-		{OptionType: layers.TCPOptionKindSACKPermitted, OptionLength: 2},
-		{OptionType: layers.TCPOptionKindTimestamps, OptionLength: 10, OptionData: make([]byte, 8)},
-		{OptionType: layers.TCPOptionKindNop},
-		{OptionType: layers.TCPOptionKindWindowScale, OptionLength: 3, OptionData: []byte{8}},
-	}
-
-	ackOptions := []layers.TCPOption{
-		{OptionType: layers.TCPOptionKindNop},
-		{OptionType: layers.TCPOptionKindNop},
-		{OptionType: layers.TCPOptionKindTimestamps, OptionLength: 10, OptionData: make([]byte, 8)},
-	}
-
 	sh := &SendHandle{
 		handle:     handle,
 		txProto:    cfg.TxProto,
 		srcPort:    uint16(cfg.Port),
-		synOptions: synOptions,
-		ackOptions: ackOptions,
+		bufOptions: gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true},
 		tcpF:       TCPF{tcpF: iterator.Iterator[conf.TCPF]{Items: cfg.TCP.LF}, clientTCPF: make(map[uint64]*iterator.Iterator[conf.TCPF])},
 		time:       uint32(time.Now().UnixNano() / int64(time.Millisecond)),
-		ethPool: sync.Pool{
-			New: func() any {
-				return &layers.Ethernet{SrcMAC: cfg.Interface.HardwareAddr}
-			},
-		},
-		ipv4Pool: sync.Pool{
-			New: func() any {
-				return &layers.IPv4{}
-			},
-		},
-		ipv6Pool: sync.Pool{
-			New: func() any {
-				return &layers.IPv6{}
-			},
-		},
-		tcpPool: sync.Pool{
-			New: func() any {
-				return &layers.TCP{}
-			},
-		},
-		udpPool: sync.Pool{
-			New: func() any {
-				return &layers.UDP{}
-			},
-		},
-		bufPool: sync.Pool{
-			New: func() any {
-				return gopacket.NewSerializeBuffer()
-			},
-		},
+		ethPool: sync.Pool{New: func() any {
+			return &layers.Ethernet{SrcMAC: cfg.Interface.HardwareAddr}
+		}},
+		ipv4Pool: sync.Pool{New: func() any { return &layers.IPv4{} }},
+		ipv6Pool: sync.Pool{New: func() any { return &layers.IPv6{} }},
+		tcpPool:  sync.Pool{New: func() any { return &layers.TCP{} }},
+		udpPool:  sync.Pool{New: func() any { return &layers.UDP{} }},
+		bufPool: sync.Pool{New: func() any {
+			return gopacket.NewSerializeBuffer()
+		}},
 	}
 	if cfg.IPv4.Addr != nil {
 		sh.srcIPv4 = cfg.IPv4.Addr.IP
@@ -130,47 +98,53 @@ func (h *SendHandle) ipProto() layers.IPProtocol {
 
 func (h *SendHandle) buildIPv4Header(dstIP net.IP) *layers.IPv4 {
 	ip := h.ipv4Pool.Get().(*layers.IPv4)
-	*ip = layers.IPv4{
-		Version:  4,
-		IHL:      5,
-		TOS:      184,
-		TTL:      64,
-		Flags:    layers.IPv4DontFragment,
-		Protocol: h.ipProto(),
-		SrcIP:    h.srcIPv4,
-		DstIP:    dstIP,
-	}
+	*ip = layers.IPv4{Version: 4, IHL: 5, TOS: 184, TTL: 64, Flags: layers.IPv4DontFragment, Protocol: h.ipProto(), SrcIP: h.srcIPv4, DstIP: dstIP}
 	return ip
 }
 
 func (h *SendHandle) buildIPv6Header(dstIP net.IP) *layers.IPv6 {
 	ip := h.ipv6Pool.Get().(*layers.IPv6)
-	*ip = layers.IPv6{
-		Version:      6,
-		TrafficClass: 184,
-		HopLimit:     64,
-		NextHeader:   h.ipProto(),
-		SrcIP:        h.srcIPv6,
-		DstIP:        dstIP,
-	}
+	*ip = layers.IPv6{Version: 6, TrafficClass: 184, HopLimit: 64, NextHeader: h.ipProto(), SrcIP: h.srcIPv6, DstIP: dstIP}
 	return ip
+}
+
+func ensureTCPOptions(tcp *layers.TCP, syn bool) {
+	if syn {
+		if cap(tcp.Options) < 5 {
+			tcp.Options = make([]layers.TCPOption, 5)
+		} else {
+			tcp.Options = tcp.Options[:5]
+		}
+		tcp.Options[0] = layers.TCPOption{OptionType: layers.TCPOptionKindMSS, OptionLength: 4, OptionData: tcpOptMSSData}
+		tcp.Options[1] = layers.TCPOption{OptionType: layers.TCPOptionKindSACKPermitted, OptionLength: 2}
+		tcp.Options[2] = layers.TCPOption{OptionType: layers.TCPOptionKindTimestamps, OptionLength: 10}
+		tcp.Options[3] = layers.TCPOption{OptionType: layers.TCPOptionKindNop}
+		tcp.Options[4] = layers.TCPOption{OptionType: layers.TCPOptionKindWindowScale, OptionLength: 3, OptionData: tcpOptWindowScaleData}
+	} else {
+		if cap(tcp.Options) < 3 {
+			tcp.Options = make([]layers.TCPOption, 3)
+		} else {
+			tcp.Options = tcp.Options[:3]
+		}
+		tcp.Options[0] = layers.TCPOption{OptionType: layers.TCPOptionKindNop}
+		tcp.Options[1] = layers.TCPOption{OptionType: layers.TCPOptionKindNop}
+		tcp.Options[2] = layers.TCPOption{OptionType: layers.TCPOptionKindTimestamps, OptionLength: 10}
+	}
+	if len(tcp.Options[2].OptionData) != 8 {
+		tcp.Options[2].OptionData = make([]byte, 8)
+	}
 }
 
 func (h *SendHandle) buildTCPHeader(dstPort uint16, f conf.TCPF) *layers.TCP {
 	tcp := h.tcpPool.Get().(*layers.TCP)
-	*tcp = layers.TCP{
-		SrcPort: layers.TCPPort(h.srcPort),
-		DstPort: layers.TCPPort(dstPort),
-		FIN:     f.FIN, SYN: f.SYN, RST: f.RST, PSH: f.PSH, ACK: f.ACK, URG: f.URG, ECE: f.ECE, CWR: f.CWR, NS: f.NS,
-		Window: 65535,
-	}
+	*tcp = layers.TCP{SrcPort: layers.TCPPort(h.srcPort), DstPort: layers.TCPPort(dstPort), FIN: f.FIN, SYN: f.SYN, RST: f.RST, PSH: f.PSH, ACK: f.ACK, URG: f.URG, ECE: f.ECE, CWR: f.CWR, NS: f.NS, Window: 65535}
 
 	counter := atomic.AddUint32(&h.tsCounter, 1)
 	tsVal := h.time + (counter >> 3)
 	if f.SYN {
-		binary.BigEndian.PutUint32(h.synOptions[2].OptionData[0:4], tsVal)
-		binary.BigEndian.PutUint32(h.synOptions[2].OptionData[4:8], 0)
-		tcp.Options = h.synOptions
+		ensureTCPOptions(tcp, true)
+		binary.BigEndian.PutUint32(tcp.Options[2].OptionData[0:4], tsVal)
+		binary.BigEndian.PutUint32(tcp.Options[2].OptionData[4:8], 0)
 		tcp.Seq = 1 + (counter & 0x7)
 		tcp.Ack = 0
 		if f.ACK {
@@ -178,76 +152,78 @@ func (h *SendHandle) buildTCPHeader(dstPort uint16, f conf.TCPF) *layers.TCP {
 		}
 	} else {
 		tsEcr := tsVal - (counter%200 + 50)
-		binary.BigEndian.PutUint32(h.ackOptions[2].OptionData[0:4], tsVal)
-		binary.BigEndian.PutUint32(h.ackOptions[2].OptionData[4:8], tsEcr)
-		tcp.Options = h.ackOptions
+		ensureTCPOptions(tcp, false)
+		binary.BigEndian.PutUint32(tcp.Options[2].OptionData[0:4], tsVal)
+		binary.BigEndian.PutUint32(tcp.Options[2].OptionData[4:8], tsEcr)
 		seq := h.time + (counter << 7)
 		tcp.Seq = seq
 		tcp.Ack = seq - (counter & 0x3FF) + 1400
 	}
-
 	return tcp
 }
 
 func (h *SendHandle) Write(payload []byte, addr *net.UDPAddr) error {
 	buf := h.bufPool.Get().(gopacket.SerializeBuffer)
 	ethLayer := h.ethPool.Get().(*layers.Ethernet)
-	defer func() {
-		buf.Clear()
-		h.bufPool.Put(buf)
-		h.ethPool.Put(ethLayer)
-	}()
 
 	dstIP := addr.IP
 	dstPort := uint16(addr.Port)
-
 	f := h.getClientTCPF(dstIP, dstPort)
 
 	var ipLayer gopacket.SerializableLayer
 	var trLayer gopacket.SerializableLayer
 	if dstIP.To4() != nil {
 		ip := h.buildIPv4Header(dstIP)
-		defer h.ipv4Pool.Put(ip)
 		ipLayer = ip
 		if h.txProto == "udp" {
 			udp := h.udpPool.Get().(*layers.UDP)
 			*udp = layers.UDP{SrcPort: layers.UDPPort(h.srcPort), DstPort: layers.UDPPort(dstPort)}
 			udp.SetNetworkLayerForChecksum(ip)
-			defer h.udpPool.Put(udp)
 			trLayer = udp
 		} else {
 			tcp := h.buildTCPHeader(dstPort, f)
 			tcp.SetNetworkLayerForChecksum(ip)
-			defer h.tcpPool.Put(tcp)
 			trLayer = tcp
 		}
 		ethLayer.DstMAC = h.srcIPv4RHWA
 		ethLayer.EthernetType = layers.EthernetTypeIPv4
 	} else {
 		ip := h.buildIPv6Header(dstIP)
-		defer h.ipv6Pool.Put(ip)
 		ipLayer = ip
 		if h.txProto == "udp" {
 			udp := h.udpPool.Get().(*layers.UDP)
 			*udp = layers.UDP{SrcPort: layers.UDPPort(h.srcPort), DstPort: layers.UDPPort(dstPort)}
 			udp.SetNetworkLayerForChecksum(ip)
-			defer h.udpPool.Put(udp)
 			trLayer = udp
 		} else {
 			tcp := h.buildTCPHeader(dstPort, f)
 			tcp.SetNetworkLayerForChecksum(ip)
-			defer h.tcpPool.Put(tcp)
 			trLayer = tcp
 		}
 		ethLayer.DstMAC = h.srcIPv6RHWA
 		ethLayer.EthernetType = layers.EthernetTypeIPv6
 	}
 
-	opts := gopacket.SerializeOptions{FixLengths: true, ComputeChecksums: true}
-	if err := gopacket.SerializeLayers(buf, opts, ethLayer, ipLayer, trLayer, gopacket.Payload(payload)); err != nil {
-		return err
+	err := gopacket.SerializeLayers(buf, h.bufOptions, ethLayer, ipLayer, trLayer, gopacket.Payload(payload))
+	if err == nil {
+		err = h.handle.WritePacketData(buf.Bytes())
 	}
-	return h.handle.WritePacketData(buf.Bytes())
+
+	if tcp, ok := trLayer.(*layers.TCP); ok {
+		h.tcpPool.Put(tcp)
+	} else if udp, ok := trLayer.(*layers.UDP); ok {
+		h.udpPool.Put(udp)
+	}
+	if ip, ok := ipLayer.(*layers.IPv4); ok {
+		h.ipv4Pool.Put(ip)
+	} else if ip, ok := ipLayer.(*layers.IPv6); ok {
+		h.ipv6Pool.Put(ip)
+	}
+	buf.Clear()
+	h.bufPool.Put(buf)
+	h.ethPool.Put(ethLayer)
+
+	return err
 }
 
 func (h *SendHandle) getClientTCPF(dstIP net.IP, dstPort uint16) conf.TCPF {
